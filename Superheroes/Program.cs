b62db525e537185ee79375fad2fad59150c5112f
Chris <chris.sewell@buildcircle.co.uk>
2022-10-12 10:26:00 +0100

    Upgrade to .Net 6

--- Superheroes/Program.cs
+++ Superheroes/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Superheroes
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
--- Superheroes/Startup.cs
+++ Superheroes/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Superheroes
 {
@@ -14,7 +15,7 @@ namespace Superheroes
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
